84b4299f77cc80513a17eb5252a7843548ea2c35
Chaeyoung714 <202102760@hufs.ac.kr>
2025-04-25 16:12:50 +0900

    mission1 : spring core 1 학습

--- spring-core-1/initial/src/test/java/cholog/BeanTest.java
+++ spring-core-1/initial/src/test/java/cholog/BeanTest.java
@@ -3,7 +3,11 @@ package cholog;
 import cholog.bean.AutowiredBean;
 import cholog.bean.SpringBean;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.StaticApplicationContext;
 
 import static cholog.utils.ContextUtils.getApplicationContext;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +24,20 @@ public class BeanTest {
     @Test
     void autowiredBean() {
         ApplicationContext context = getApplicationContext();
+        AutowiredBean autowiredBean = context.getBean("autowiredBean", AutowiredBean.class);
+        assertThat(autowiredBean.sayHello()).isEqualTo("Hello");
+    }
+
+    @Test
+    void autowiredBean2() {
+        StaticApplicationContext context = new StaticApplicationContext();
+        context.registerBeanDefinition("springBean", new RootBeanDefinition(SpringBean.class));
+
+        BeanDefinition helloDef = new RootBeanDefinition(AutowiredBean.class);
+        // 아이디가 printer 인 빈을 찾아서 printer 프로퍼티에 DI
+        helloDef.getPropertyValues().addPropertyValue("springBean", new RuntimeBeanReference("springBean"));
+        context.registerBeanDefinition("autowiredBean", helloDef);
+
         AutowiredBean autowiredBean = context.getBean("autowiredBean", AutowiredBean.class);
         assertThat(autowiredBean.sayHello()).isEqualTo("Hello");
     }
